54feaf98499302fea6830f628b60c5be72dc7d1c
Suryakumar Selvakumar <ssuryakumar2077@gmail.com>
2024-09-08 13:20:31 -0600

    Create all functions for LinkedList class and create a main.js

--- linkedList.js
+++ linkedList.js
@@ -1,8 +1,8 @@
 import { Node } from "./node";
 
 export class LinkedList {
-  constructor(head) {
-    this.head = new Node(head);
+  constructor() {
+    this.head = new Node();
   }
 
   append(value) {
@@ -21,9 +21,9 @@ export class LinkedList {
   }
 
   size() {
-    let count = 0;
-    let tmp = this.head;
-    while (tmp.nextNode != null) {
+    let count = 0,
+      tmp = this.head;
+    while (tmp != null) {
       count++;
       tmp = tmp.nextNode;
     }
@@ -43,14 +43,77 @@ export class LinkedList {
   }
 
   at(index) {
-    let count = 0;
-    let tmp = this.head;
-    while (tmp.nextNode != null) {
+    let count = 0,
+      tmp = this.head;
+    while (tmp != null) {
       if (count === index) {
         return tmp;
       }
       count += 1;
       tmp = tmp.nextNode;
+    }
+  }
+
+  pop() {
+    let curNode = this.head,
+      prevNode = null;
+    while (curNode.nextNode != null) {
+      prevNode = curNode;
+      curNode = curNode.nextNode;
+    }
+    prevNode.nextNode = null;
+  }
+
+  contains(value) {
+    let tmp = this.head;
+    while (tmp.nextNode != null) {
+      if (tmp.value == value) {
+        return true;
+      }
+      tmp = tmp.nextNode;
+    }
+    return false;
+  }
+
+  find(value) {
+    let count = 0,
+      tmp = this.head;
+    while (tmp != null) {
+      if (tmp.value == value) {
+        return count;
+      }
+      count += 1;
+      tmp = tmp.nextNode;
+    }
+    return null;
+  }
+
+  insertAt(value, index) {
+    let curNode = this.head,
+      prevNode = null,
+      count = 0;
+    while (curNode != null) {
+      if (count == index) {
+        prevNode.nextNode = new Node(value, curNode);
+      }
+      count += 1;
+      prevNode = curNode;
+      curNode = curNode.nextNode;
+    }
+  }
+
+  removeAt(index) {
+    let curNode = this.head,
+      prevNode = null,
+      count = 0;
+    while (curNode != null) {
+      if (count == index) {
+        prevNode.nextNode = curNode.nextNode;
+        break;
+      }
+      count += 1;
+      prevNode = curNode;
+      curNode = curNode.nextNode;
     }
   }
 }
